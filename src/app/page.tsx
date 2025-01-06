efcd41d34800e37ce09c226064dabc1daaa70122
Bassel Ahmed ABO-Khabsa <baakswaiengineer@gmail.com>
2025-01-07 00:43:55 +0300

    add fonts | fix scss files | add constants folder for pages's head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 const Home = (): React.JSX.Element => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>dsa</h1>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/constants/headers.ts
@@ -0,0 +1,8 @@
+export const global = {
+  title: {
+    default: "Pharmazone",
+    template: "%s | Pharmazone",
+  },
+  description:
+    "Pharmazone is the ultimate platform designed for pharmacists and pharmaceutical factories. It simplifies the ordering process, allowing pharmacies to easily place orders for pharmaceutical goods directly from factories. The platform also helps manage payments, track orders, and stay updated on factory offers. For factories, Pharmazone provides a seamless way to showcase their products, manage offers, and fulfill orders efficiently.",
+};
